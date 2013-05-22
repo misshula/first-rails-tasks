79612645b0a01480f0579230e3a8681f193ecf8b
David Misshula <david.misshula@gmail.com>
2013-05-22 12:07:51 -0400

    first rails tasks app!

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,33 @@
 class TasksController < ApplicationController
   def index
+  	@task = Task.new
+  	@tasks = Task.all
   end
+
+  def create
+  	Task.create params[:task]
+  	redirect_to :back
+  end
+
+  def edit
+  	@task = Task.find params[:id]
+  end
+
+  def update
+  	task = Task.find params[:id]
+  	if task.update_attributes params[:task]
+  		redirect_to tasks_path, :notice => 'Successfully updated task'
+  	else
+  		redirect_to :back, :notice => 'There was an error updating'
+	end
+  end
+
+  def destroy
+  	Task.destroy params[:id]
+  	redirect_to :back, :notice => 'Deleted task'
+  end
+
+
+
+
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe "Tasks" do
   describe "GET /tasks" do
-  	it "display some tasks" do 
+  	it "display some tasks" do
+  		@task = Task.create :task => 'go to bed'
+
   		visit tasks_path
   		page.should have_content 'go to bed'
   	end
